6e198329e5a2cfc753f4799f5fac65a8dec04828
Jullia <julliabeatriz@hotmail.com.br>
2022-11-09 01:06:39 -0300

    arrumando tudo

--- scripts.js
+++ scripts.js
@@ -1,32 +1,58 @@
 const currentOperation = document.querySelector('#current-operation');
 const previousOperation = document.querySelector('#previous-operation');
 const buttons = document.querySelectorAll('.btns button');
+const operations = document.querySelectorAll('.op');
 
-function insert(valor){
-    //Exclui o 0 inicial e adiciona o valor digitado direto 
-    if(valor === '.') {    
-        currentOperation.innerHTML += '.';
-    } else if (currentOperation.innerHTML === '0'){
-        currentOperation.innerHTML = valor;
+let num1;
+let num2;
+let op;
+
+function insert(value){         //tenta criar uma função pra calc e nela vc apaga o currentoperation.innerText
+    // Checa se o valor atual ja tem um ponto
+    
+    if(value === '.' && currentOperation.innerText.includes(".")){
+        return;
+    }
+    
+    if (value === '/'|| value ==='*'|| value === '-'|| value === '+'){
+        previousOperation.innerText = currentOperation.textContent + ` ${value}`;
+        currentOperation.innerText = null; //arrumar isso aqui
+        num1 = previousOperation.innerText.substring(0, previousOperation.innerText.length -2);
+        op = value;
+        return
+    }
+    
+    if(value === '.') {    
+        currentOperation.innerText += '.'; // O '0' inicial permanece e é adicionado um ponto
+    } else if (currentOperation.innerText === '0'){ // Exclui o 0 inicial e adiciona o value digitado direto
+        currentOperation.innerText = value;
     } else {
-        currentOperation.innerHTML += valor;
+        currentOperation.innerText += value;
     } 
 }
 
+console.log(num1);
+
+function calc(operation){
+
+    currentOperation.innerText = null;
+
+}
+
 function clearAll(){
-    currentOperation.innerHTML = '0';
+    currentOperation.innerText = '0';
+    previousOperation.innerText = '';
 }
 
 function backspace(){
     
     if(currentOperation.textContent){
-        let result = document.getElementById('current-operation').innerHTML
-        result = result.substring(0, result.length -1); 
-        if(result.length>0) {
-            currentOperation.innerHTML = result;
+        let valueText = document.getElementById('current-operation').innerText;
+        valueText = valueText.substring(0, valueText.length -1); 
+        if(valueText.length>0) {
+            currentOperation.innerText = valueText;
         } else {
-            currentOperation.innerHTML = '0';
+            currentOperation.innerText = '0';
         }  
     } 
 }
-
